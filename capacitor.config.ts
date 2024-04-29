b7f8c9de434d77114da3421730a5d35e9dfbb92a
Emilio Robles <ejroblesl24@gmail.com>
2024-04-29 12:04:19 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.prograhibridaunidadii',
+  appName: 'prograhibridaunidadii',
+  webDir: 'www'
+};
+
+export default config;
